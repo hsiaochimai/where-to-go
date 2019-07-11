706fed7c2d3d8caca77aca5a54ee19d59b7ce1a9
hsiaochimai <jchmai21@gmail.com>
2019-07-11 05:17:44 -0700

    fixed edit trip

--- src/components/EditTrip/EditTrip.js
+++ src/components/EditTrip/EditTrip.js
@@ -1,49 +1,53 @@
 import React, { Component } from "react";
 import ControlledInput from "../ControlledInput/ControlledInput";
 
-export default class EditTrip extends Component{
-    constructor(props) {
-        super(props);
-        const {trip} = this.props
-        this.state={
-            ...trip
-        }
-    
-      }
-    render(){
-      
-        console.log(`edit trip`, this.props.trip)
-        // const{
-        //     name,
-        //     numOfdays
-        // }=this.props.trip
-        return(<div>
-            <form>
+export default class EditTrip extends Component {
+  constructor(props) {
+    super(props);
+    const { trip } = this.props;
+    this.state = {
+      trip: { ...trip }
+    };
+  }
+  onChange = (fieldName, value) => {
+    const changedTrip = { ...this.state.trip, [fieldName]: value };
+    this.setState({ trip: changedTrip }, () => {
+      console.log("state changed:", JSON.stringify(this.state.trip, 2, 2));
+    });
+  };
+  render() {
+    console.log(`edit trip`, this.state);
+    const { name, numOfDays } = this.state.trip;
+
+    return (
+      <div>
+        <form>
           <p>
             <span>Name</span>
-                <ControlledInput
-                //   onChange={value => this.onChange("full_name", value)}
-                  tag="input"
-                  type="text"
-                  required={true}
-                  initialValue={this.props.trip.name}
-                //   editMode={editMode}
-                />
-              
+            <ControlledInput
+              onChange={value => this.onChange("name", value)}
+              tag="input"
+              type="text"
+              required={true}
+              initialValue={name}
+              //   editMode={editMode}
+            />
           </p>
           <p>
             <span>Duration of Stay</span>
-                <ControlledInput
-                //   onChange={value => this.onChange("full_name", value)}
-                  tag="input"
-                  type="number"
-                  required={true}
-                  initialValue={this.props.trip.numOfDays}
-                //   editMode={editMode}
-                />
-              days
+            <ControlledInput
+                onChange={value => this.onChange("numOfDays", value)}
+              tag="input"
+              type="number"
+              min="1"
+              required={true}
+              initialValue={numOfDays}
+              //   editMode={editMode}
+            />
+            days
           </p>
-          </form>
-        </div>)
-    }
+        </form>
+      </div>
+    );
+  }
 }
