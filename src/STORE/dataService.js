81d70fbc276c188f066a853bd6acc1962f42e03e
hsiaochimai <jchmai21@gmail.com>
2019-07-11 05:32:53 -0700

    added local storage

--- src/STORE/dataService.js
+++ src/STORE/dataService.js
@@ -1,38 +1,54 @@
 import store from "./store";
+import ls from "local-storage";
+
+//set this to an user object to login
 let user;
 const ds = {
+  persistLoginData: async data => {
+    return ls("authData", data);
+  },
+  autoLogin: async () => {
+    const data = ls("authData");
+    return data;
+  },
   doLogin: async (email, password) => {
     user = store.users.find(
       user => user.email === email && user.password === password
     );
+    if (user) {
+      ds.persistLoginData(user);
+    }
     return user;
   },
   getLoggedInUser: async () => {
-    return user
+    const savedUser = await ds.autoLogin();
+    if (!user && savedUser) {
+      user = savedUser;
+    }
+    return user;
   },
 
-
-  deleteTrip: async (id) => {
-    const { trips } = store
-    trips.splice(trips.findIndex(t => t.id === id), 1)
+  deleteTrip: async id => {
+    const { trips } = store;
+    trips.splice(trips.findIndex(t => t.id === id), 1);
     // adding a trip
     // trips.push({id, xx, name:'Foo' ,.......})
   },
 
   //userID will be token after implementiing JWT tokens
   getTrips: async () => {
-    console.log(`ds:getTrips(${user.email})`)
+    console.log(`ds:getTrips(${user.email})`);
     const trips = store.trips.filter(trip => trip.user_id === user.id);
     trips.forEach(trip => {
       //associated records
-      trip.places = store.places.filter(place => place.trip_id === trip.id)
-    })
-    return trips
+      trip.places = store.places.filter(place => place.trip_id === trip.id);
+    });
+    return trips;
   },
-  deletePlace: async (id) => {
-    const { places } = store
-    places.splice(places.findIndex(p => p.id === id), 1)
-    console.log(`did the delete work`,places)
+  deletePlace: async id => {
+    const { places } = store;
+    places.splice(places.findIndex(p => p.id === id), 1);
+    console.log(`did the delete work`, places);
   }
 };
 
