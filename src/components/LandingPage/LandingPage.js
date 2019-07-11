ff139892f2fb55ca77785b2ec0cf002bd9a2b690
hsiaochimai <jchmai21@gmail.com>
2019-07-11 15:10:29 -0700

    landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
+import './LandingPage.css'
 export default class LandingPage extends Component{
     render(){
         return(
             <div>
                  <NavBar/>
-                 <div className='landing-page'>
-                <h1>Welcome to Where To Go!</h1>
-          <p>Get started by adding a new trip!</p>
+                 <div className='landing-page flex-column '>
+                <h1 >Welcome to Where To Go!</h1>
+          <p >Get started by adding a new trip!</p>
          <Link to='/add-trip'> <button>Add Trip</button></Link>
          </div>
             </div>
