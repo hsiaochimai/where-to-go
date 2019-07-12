935a1f0351076eb39d7dc5461b29fd7508f2045b
hsiaochimai <jchmai21@gmail.com>
2019-07-12 06:44:34 -0700

    card css

--- src/components/Trips/PlaceList.js
+++ src/components/Trips/PlaceList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Card, CardHeader, CardBody, CardFooter } from "react-simple-card";
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 import ds from '../../STORE/dataService'
-
+import './PlaceList.css'
 import pt from 'prop-types'
 // import whereToGoContext from '../whereToGoContext/whereToGoContext'
 const {deletePlace} = ds
@@ -19,17 +20,25 @@ export default class PlaceList extends Component {
         const tripPlaces = trip.places
 
         const placeCard = tripPlaces.map((place, i) => {
-            const card = <Card key={i}>
+            const card = 
+                 <div className='placeCard'>
+                     <div className='action-buttons'>
+                         
                  <button>Edit</button>
                  <button onClick={()=>deletePlace(place.id)} >Delete</button>
-                <CardHeader>Name: {place.name}</CardHeader>
-                <CardBody><li>Addresss: {place.street_address}</li>
-                    <li>City: {place.city}</li>
-                    <li>Transporation: {place.transportation}</li>
-                    <p>Notes: {place.notes}</p>
-                </CardBody>
-                <CardFooter>{place.id}</CardFooter>
-            </Card>
+                 </div>
+                <div className="cardContent padded">
+                 <h2> {place.name}</h2> 
+               <ul className='padded'>
+                <li className='padded'>Addresss: {place.street_address}</li>
+                    <li className='padded'>City: {place.city}</li>
+                    <li className='padded'>Transporation: {place.transportation}</li>
+                    <p className='padded'>Notes: {place.notes}</p>
+                    </ul>
+                    </div>
+                
+                </div>
+            
             return card
         })
         return (
