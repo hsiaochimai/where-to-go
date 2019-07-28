8db02c118937bc0d84ed489eeb32530d1e66ba26
hsiaochimai <jchmai21@gmail.com>
2019-07-28 10:30:22 -0400

    fixed large trip list

--- src/components/TripList/TripList.js
+++ src/components/TripList/TripList.js
@@ -27,7 +27,7 @@ export default class TripList extends Component{
         })
         const largeTrip=[...this.props.trips]
         const largeTripList=largeTrip.map((trip, i)=>{
-            return <h2 key={i} className='padded '> <a key={i} className='padded ' onClick={(e)=>{this.props.onTripSelected(trip.id)}} href="#">{trip.name}</a></h2>
+            return <li key={i} className='padded '> <a key={i} className='padded ' onClick={(e)=>{this.props.onTripSelected(trip.id)}} href="#">{trip.name}</a></li>
         })
         console.log(this.state)
         return(
@@ -36,7 +36,11 @@ export default class TripList extends Component{
         <div className='tripList-select'><Select options={tripList}
         onChange={e => this.props.onTripSelected(e.value)}/></div>
          <div className='largeTripList'>
+             <nav>
+                 <ul>
           {largeTripList}
+          </ul>
+          </nav>
           </div>
             </div>
             
